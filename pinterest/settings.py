330031e64d9174eb7ccb8ab3d7c1b912c5319360
Brian Salomaki <brian@salomaki.com>
2017-04-24 21:05:12 -0500

    pg migrate

--- pinterest/settings.py
+++ pinterest/settings.py
@@ -75,8 +75,14 @@ WSGI_APPLICATION = 'pinterest.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'pin_db',
+        'USER': 'pin_admin',
+        'PASSWORD': 'pin2017',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
