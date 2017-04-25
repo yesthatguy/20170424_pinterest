55e4f26df41e2ce6f1f48d0a5364ffeeacdecbc9
Brian Salomaki <brian@salomaki.com>
2017-04-24 21:43:13 -0500

    jugar con forms

--- modules/users/forms.py
+++ modules/users/forms.py
@@ -1,4 +1,8 @@
 from django import forms
 
 class SignupForm(forms.Form):
-    your_name = forms.CharField(label='Your name', max_length=100)
+    your_name = forms.CharField(label='Tu nombre', max_length=100)
+    ciudad = forms.CharField(label='Tu ciudad', max_length=50)
+    email = forms.EmailField()
+    fecha = forms.DateField()
+    mensaje = forms.CharField(label='Mensaje', widget=forms.Textarea)
